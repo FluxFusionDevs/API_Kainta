7f69fc2819e1040b0040821bd68a92235c5d88f6
ralphjenrey <loquellanoralphjenrey@gmail.com>
2024-11-19 23:38:57 +0800

    added food routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const establishmentRoutes = require('./establishmentRoutes');
 const ratingRoutes = require('./ratingRoutes');
 const barangayRoutes = require('./barangayRoutes');
 const router = express.Router();
+const foodRoutes = require('./foodRoutes');
 
 router.get('/', (req, res) => {
     res.send('Hello World');
@@ -13,4 +14,5 @@ router.use('/users', userRoutes);
 router.use('/establishments', establishmentRoutes);
 router.use('/ratings', ratingRoutes);
 router.use('/barangays', barangayRoutes);
+router.use('/foods', foodRoutes);
 module.exports = router;
