e9fbfbe35bb140816cb80bc4b632e245049a0ff9
ralphjenrey <loquellanoralphjenrey@gmail.com>
2024-11-21 12:07:15 +0800

    config middleware upload to support multiple fields

--- src/routes/establishmentRoutes.js
+++ src/routes/establishmentRoutes.js
@@ -3,20 +3,42 @@ const router = express.Router();
 const establishmentController = require("../controllers/establishmentController");
 const { createUploadMiddleware } = require("../middleware/upload");
 
-const uploadDocument = createUploadMiddleware({
-    directory: 'uploads/documents',
-    filePrefix: 'document',
-    fieldName: 'image',
-    maxSize: 5 * 1024 * 1024 // 5MB
-});
+const uploadFiles = createUploadMiddleware({
+    fields: [
+      {
+        directory: "uploads/documents",
+        filePrefix: "document",
+        fieldName: "documentImage",
+        maxSize: 5 * 1024 * 1024, // 5MB
+      },
+      {
+        directory: "uploads/establishments",
+        filePrefix: "establishment",
+        fieldName: "establishmentImage",
+        maxSize: 5 * 1024 * 1024, // 5MB
+      }
+    ]
+  });
 
 router.get("/get-establishments", establishmentController.getEstablishments);
 router.get("/get-establishment", establishmentController.getEstablishmentById);
-router.post("/create-establishment", establishmentController.createEstablishment);
-router.put("/update-establishment", establishmentController.updateEstablishment);
-router.delete("/delete-establishment", establishmentController.deleteEstablishment);
+router.post(
+  "/create-establishment",
+  uploadFiles,
+  establishmentController.createEstablishment
+);
+router.put(
+  "/update-establishment",
+  establishmentController.updateEstablishment
+);
+router.delete(
+  "/delete-establishment",
+  establishmentController.deleteEstablishment
+);
 router.get("/get-barangays", establishmentController.getBarangays); // New route to get barangays
-router.get("/get-establishment-by-barangay", establishmentController.getEstablishmentByBarangay);
-router.post("/upload-document", uploadDocument, establishmentController.uploadDocument);
+router.get(
+  "/get-establishment-by-barangay",
+  establishmentController.getEstablishmentByBarangay
+);
+// router.post("/upload-document", uploadDocument, establishmentController.uploadDocument);
 module.exports = router;
-
