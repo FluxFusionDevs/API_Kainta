5096629400c119df6d6b10d756dc0c7ca43c67b3
ralphjenrey <loquellanoralphjenrey@gmail.com>
2024-11-30 10:19:48 +0800

    added token validation

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const passport = require('../passport');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-
+const auth = require('../middleware/auth');
 const userController = require('../controllers/userController');
 
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'], failureRedirect: '/' }));
@@ -17,6 +17,8 @@ router.post('/login', userController.loginWithEmailAndPassword);
 router.post('/register', userController.registerWithEmailAndPassword);
 router.post('/login-with-google', userController.loginWithGoogle);
 router.post('/login-with-google-web', userController.loginWithGoogleWeb);
-
+router.post('/validate-token', auth, (req, res) => {
+  res.status(200).send('Token is valid');
+});
 
 module.exports = router;
